6582db6ed518f30e1edc7102ed1c279e9c096a87
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-10-28 19:00:04 +0000

    update the return position in eqArrays function to outside of the for loop

--- eqArrays.js
+++ eqArrays.js
@@ -18,12 +18,14 @@ const eqArrays = (actual, expected) =>{
   if (actual.length !== expected.length){
     return false;
   }
+  
   for (let i = 0; i < actual.length; i++){
     if (actual[i] !== expected[i]){
       return false;
     }
-    return true;
+    
   }
+  return true;
 }
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
--- without.js
+++ without.js
@@ -4,12 +4,14 @@ const eqArrays = (actual, expected) =>{
   if (actual.length !== expected.length){
     return false;
   }
+  
   for (let i = 0; i < actual.length; i++){
     if (actual[i] !== expected[i]){
       return false;
     }
-    return true;
+    
   }
+  return true;
 }
 const assertArraysEqual = (actual, expected) =>{
   if (eqArrays (actual, expected) === true) {
