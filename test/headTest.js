2117ab71d3477ac60e6973bb4d2b31ba798c9ef6
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-02 02:29:35 +0000

    incorporate mocha and chai and use describe and it to update the test code

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,23 @@
 
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-// test code
+//const assertEqual = require('../assertEqual');
+
+/* test code
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 console.log(head([]));
 console.log(head([1]));
+*/
+
+
+const head = require('../head');
+const assert = require('chai').assert;
+describe("#head",() => {
+  it("reutrns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for ['Hello','L','Lab']", () => {
+    assert.strictEqual(head(['Hello','L','Lab']), 'Hello');
+  })
+});
