1947290c9c7f05fa7aedd80aff2a8431a9983138
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-02 01:43:01 +0000

    move test code to tailTest.js and use module to import and export function

--- tail.js
+++ tail.js
@@ -1,31 +1,9 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`👎Assertion Failed: "${actual}" !== "${expected}"`);
-  }
+const assertEqual = require('./assertEqual');
 
-};
-
-let tail = array => {
+const tail = array => {
   let newArr = array.slice(1);
   return newArr;
 };
 
-// Test Case 1: Check the returned array elements
-let r = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(r, ["Lighthouse", "Labs"]); // => will always fail!
-//Instead of comparing arrays, a workaround to this limitation could be to compare the values of the returned tail array directly
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([1]));
-console.log(tail([]));
+module.exports = tail;
